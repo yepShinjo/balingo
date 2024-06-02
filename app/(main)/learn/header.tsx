a91092ec40ca1170507bd98ff8785dfb65e643df
yepShinjo <backguy05@gmail.com>
2024-06-03 02:25:38 +0800

    page wrapper, its 2 am in the morning

---
+++ app/(main)/learn/header.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+type Props = {
+    title: string;
+}
+
+// export default const... (we have more than one const inside the file (usage : exporting multiple const))
+// export const... (we only have one const inside the file)
+export const Header = ({ title }: Props) => {
+    return (
+        // lg:z-50 why?
+        // cuz on large we gon have some other floating elements that CAN overlap with header
+
+        // 2 actual components inside the div (Link, h1). Added an extra self closing div because the arrowLeft icon is so small and the text is also small
+        // justify-between align all element and equally space them out
+        // so, to trick that, add an empty div 
+        <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
+            <Link href="/courses">
+                <Button variant="ghost" size="sm"> 
+                    <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400"/> 
+                </Button>
+            </Link>
+            <h1 className="font-bold text-lg">
+                {title}
+            </h1>
+            <div />
+        </div>
+    )
+}
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { FeedWrapper } from "@/components/ui/feed-wrapper"
+import { StickyWrapper } from "@/components/ui/sticky-wrapper"
+import { Header } from "./header"
+import { UserProgress } from "@/components/ui/user-progress"
+
 const LearnPage = () => {
     return (
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{ title: 'Japanese', imageSrc: '/jp.svg' }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                >
+                </UserProgress>
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Japanese" />
+            </FeedWrapper>
         </div>
     )
 }
