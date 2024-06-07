6b6580a40428b22a5ee729630c8031111c4e7dd2
yepShinjo <backguy05@gmail.com>
2024-06-07 19:48:19 +0800

    added a lot of schema and devReadme about DB

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     // there are other types for PK such as integer, uuid, etc, but we choose serial so it auto increment
@@ -10,8 +10,98 @@ export const courses = pgTable("courses", {
 
 // for all the courses that the user is progressing
 export const coursesRelations = relations(courses, ({ many }) => ({
-    userProgress : many(userProgress),
+    // courses can have many userProgress
+    userProgress : many(userProgress), 
+    // courses can have many units
+    units: many(units),
 }))
+
+
+export const units = pgTable("units", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(), // bagian 1
+    description: text("description").notNull(), // melajah basa bali Sor se wai wai
+    courseId: integer("course_id").references(() => courses.id, { onDelete: "cascade"}).notNull(),
+    order: integer("order").notNull(),
+})
+
+// units will have a one to one relationship with courses
+// units will have a one to many relationship with lessons
+export const unitsRelations = relations(units, ({ many, one }) => ({
+    course: one(courses, {
+        fields: [units.courseId],
+        references: [courses.id],
+    }),
+    lesson: many(lessons),
+}))
+
+export const lessons = pgTable("lessons", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    unitId: integer("unit_id").references(() => units.id, { onDelete: "cascade"}).notNull(),
+    order: integer("order").notNull(),
+})
+
+// lessons will have a one to many relationship with units
+// lessons will have a many to one relationship with challenges
+export const lessonsRelations = relations(lessons, ({ many, one }) => ({
+    units: one(units, {
+        fields: [lessons.unitId],
+        references: [units.id],
+    }),
+    challenges: many(challenges)
+}))
+
+export const challengesEnum = pgEnum("type", ["SELECT", "ASSIST"])
+
+export const challenges = pgTable("challenges", {
+    id: serial("id").primaryKey(),
+    lessonId: integer("lesson_id").references(() => lessons.id, { onDelete: "cascade"}).notNull(),
+    type: challengesEnum("type").notNull(),
+    question: text("question").notNull(),
+    order: integer("order").notNull(),
+})
+
+export const challengeRelations = relations(challenges, ({ many, one }) => ({
+    lessons: one(lessons, {
+        fields: [challenges.lessonId],
+        references: [lessons.id],
+    }),
+    challengeOptions: many(challengeOptions),
+    challengeProgress: many(challengeProgress),
+}))
+
+// answers option
+export const challengeOptions = pgTable("challengeOptions", {
+    id: serial("id").primaryKey(),
+    challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade"}).notNull(),
+    text: text("text").notNull(),
+    correct: boolean("correct").notNull(),
+    imageSrc: text("image_src"),
+    audioSrc: text("audio_src"),
+})
+
+export const challengeOptionsRelations = relations(challengeOptions, ({ one }) => ({
+    challenges: one(challenges, {
+        fields: [challengeOptions.id],
+        references: [challenges.id],
+    })
+}))
+
+export const challengeProgress = pgTable("challengeProgress", {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(), // TODO : please dont break (better confirm it doesnt break)
+    challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade"}).notNull(),
+    completed: boolean("completed").notNull().default(false),
+})
+
+export const challengeProgressRelations = relations(challengeProgress, ({ one }) => ({
+    challenges: one(challenges, {
+        fields: [challengeProgress.id],
+        references: [challenges.id],
+    })
+}))
+
 
 export const userProgress = pgTable("user_progress", {
     userId: text("user_id").primaryKey(),
--- devReadme.md
+++ devReadme.md
@@ -43,11 +43,88 @@ The confusion arises because:
 
 #### To avoid confusion, it might be helpful to think of many and one in these definitions as referring to how many records in the related table (userProgress or courses) are associated with a single record in the source table (courses or userProgress), respectively.
 
+#### Example
+    // relationship name is courses. relationship holder is coursesRelations. courses can have "many"
+    export const coursesRelations = relations(courses, ({ many }) => ({
+        // courses can have many userProgress
+        userProgress : many(userProgress), 
+        // courses can have many units
+        units: many(units),
+    }))
+
 
 *1. Relationship Name : **userProgress** | Relationship holder = **coursesRelations**
 description :
 courses table with userProgress table - one to many
 
+
 *2. Relationship Name : **activeCourse** | Relationship holder = **userProgressRelations**
 description :
 courses table with userProgress table - one to one
+
+*3. Relationship Name : **course** | Relationship holder = **unitRelations**
+description :
+units table with courses table - one to one
+
+Base Table: units
+Relationship Name: course
+Type of Connection: one (indicating a one-to-one or one-to-many relationship from the perspective of units)
+in this example | course: one(courses, { | it is a one to one.
+
+End Table: courses
+Fields Connecting:
+
+    fields: [units.courseId] from the units table
+    references: [courses.id] from the courses table
+
+
+## perfect explanation
+
+
+    export const courses = pgTable("courses", {
+        id: serial("id").primaryKey(),
+        title: text("title").notNull(),
+        imageSrc: text("image_src").notNull(),
+    })
+
+    export const coursesRelations = relations(courses, ({ many }) => ({
+        userProgress : many(userProgress), 
+        units: many(units),
+    }))
+
+    export const units = pgTable("units", {
+        id: serial("id").primaryKey(),
+        title: text("title").notNull(),
+        description: text("description").notNull(),
+        courseId: integer("course_id").references(() => courses.id, { onDelete: "cascade"}).notNull(),
+        order: integer("order").notNull(),
+    })
+
+    export const unitsRelations = relations(units, ({ many, one }) => ({
+        course: one(courses, {
+            fields: [units.courseId],
+            references: [courses.id],
+        }),
+        lesson: many(lessons),
+    }))
+
+    export const lessons = pgTable("lessons", {
+        id: serial("id").primaryKey(),
+        title: text("title").notNull(),
+        unitId: integer("unit_id").references(() => units.id, { onDelete: "cascade"}).notNull(),
+        order: integer("order").notNull(),
+    })
+
+Here we have "courses" "units" "lessons" table
+
+first relation coursesRelations:
+connecting **courses** with **userProgress** => one courses can have many userProgress
+
+second relation unitsRelations :
+connecting **units** with **courses** => one units can only have one courses (an unit is only attached to one courses), and
+
+connecting **units** with **lessons** => one units can have many lessons
+
+notice in the above type of connection and examples we can only have "one to one" or "one to many" because it is always seen from the "Base Table"
+one Base Table can have {one} relation with end table, or
+one Base Table can have {many} relation with end table. Thats all
